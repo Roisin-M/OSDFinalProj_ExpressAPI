2ae38ab9ce96c04fd98e5783246c588d4240f9ee
roisin-m <s00223865@atu.ie>
2024-10-15 18:38:04 +0100

    installed nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, {Application, Request, Response} from "express";
+//import morgan from "morgan";
 
 const PORT = process.env.Port || 3000;
 
 const app: Application = express();
+
+//app.use(morgan("tiny"));
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
