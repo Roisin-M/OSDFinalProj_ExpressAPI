1e060cbb9d0ddc518523b232b5240ff19babd1bf
roisin-m <s00223865@atu.ie>
2024-12-20 12:25:20 +0000

    regex for user password in joi validation

--- src/models/user.ts
+++ src/models/user.ts
@@ -15,7 +15,18 @@ export const ValidateUser = (user: User)=>{
     name: Joi.string().min(3).required(),
     phonenumber: Joi.string().min(10),
     email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(64).required(),
+    password: Joi.string()
+    .min(8)
+    .max(64)
+    .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/)
+    .required()
+    .messages({
+      "string.pattern.base": 
+          "Password must contain at least one uppercase letter, one number, and one special character (!@#$%^&*).",
+        "string.min": "Password must be at least 8 characters long.",
+        "string.max": "Password must not exceed 64 characters.",
+        "string.empty": "Password is required.",
+    }),
     });
     return userJoiSchema.validate(user, { abortEarly: false });
   //all joi validation errors are displayed instead of stopping and displaying just the first one
